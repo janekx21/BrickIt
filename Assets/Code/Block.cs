d03552aaf89e6f06066c6a57f9331c902f2aad1f
Janek <janekx21@gmail.com>
2019-11-20 20:45:58 +0100

    add colored Bock

--- Assets/Code/Block.cs
+++ Assets/Code/Block.cs
@@ -2,11 +2,13 @@
 
 [RequireComponent(typeof(SpriteRenderer), typeof(Rigidbody2D))]
 public abstract class Block : Entity {
-	private Rigidbody2D rig;
+	protected Rigidbody2D rig = null;
+	protected SpriteRenderer ren = null;
 
 	public override void Awake() {
 		base.Awake();
 		rig = GetComponent<Rigidbody2D>();
+		ren = GetComponent<SpriteRenderer>();
 	}
 
 	public virtual void Hit(IActor maker) {
---
+++ Assets/Code/ColoredBlock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColoredBlock : Block {
+	[SerializeField] private Color color = Color.white;
+
+	public override void Update() {
+		base.Update();
+		ren.color = color;
+	}
+
+	public override void Hit(IActor maker) {
+		base.Hit(maker);
+		if (maker.GetColor() == color) {
+			Break();
+		}
+	}
+}
