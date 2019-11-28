aa9e3cab797b4be26f5507a816ee9a8b303a7b2a
Janek <janekx21@gmail.com>
2019-11-28 12:41:56 +0100

    level scriptable Object

---
+++ Assets/Editor/LevelObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[CreateAssetMenu]
+public class LevelObject : ScriptableObject {
+    public string levelName = "noname";
+    public string levelAuthor = "no one";
+    public SceneAsset scene = null;
+}
