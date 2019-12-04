7fa46ffb9f5fab5c21b1ca9068a676995df2e340
Maico Weise <silevol@gmail.com>
2019-12-04 21:27:55 +0100

    added BlockColor superclass

---
+++ Assets/Code/BlockColor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BlockColor : Block {
+
+    [SerializeField] private Color color = Color.white;
+
+    private void OnDrawGizmos() {
+        Gizmos.color = color;
+        for (float i = .8f; i < 1f; i += .01f) {
+            Gizmos.DrawWireCube(transform.position, new Vector3(1, 1, 0) * i);
+        }
+    }
+
+    protected Color GetColor() {
+        return color;
+    }
+
+    protected void SetColor(Color color) {
+        this.color = color;
+    }
+}
