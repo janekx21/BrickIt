c25c4c64f9dd101fadf33e41d256f88e04a4f362
Janek Winkler <janekx21@gmail.com>
2020-02-20 21:50:46 +0100

    new random sprites

---
+++ Assets/Code/RandomSprite.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class RandomSprite : MonoBehaviour {
+    [SerializeField] Sprite[] possibleSprites = new Sprite[0];
+
+    public void Start() {
+        var ren = GetComponent<SpriteRenderer>();
+        
+        var position = transform.position;
+        int index = Mathf.FloorToInt(position.x * 131 + position.y * 17);
+        ren.sprite = possibleSprites[index % possibleSprites.Length];
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawIcon(transform.position, "CubeInCircle.png");
+    }
+}
