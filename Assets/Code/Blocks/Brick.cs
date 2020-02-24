1ff4d98666ebb66f4d2ed284af331889d69bbf7e
Janek Winkler <janekx21@gmail.com>
2020-02-24 17:32:02 +0100

    coding style

--- Assets/Code/Blocks/Brick.cs
+++ Assets/Code/Blocks/Brick.cs
@@ -1,5 +1,5 @@
 namespace Blocks {
-	public class Brick : Block {
-		protected override bool shouldBreak() => true;
-	}
+    public class Brick : Block {
+        protected override bool shouldBreak() => true;
+    }
 }
