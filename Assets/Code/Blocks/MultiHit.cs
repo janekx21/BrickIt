4fd1068024231e957ca8eabe6cea72df32e983fb
Janek Winkler <janekx21@gmail.com>
2020-02-25 20:34:34 +0100

    fix bug multihit block color match

--- Assets/Code/Blocks/MultiHit.cs
+++ Assets/Code/Blocks/MultiHit.cs
@@ -2,28 +2,30 @@
 using UnityEngine;
 
 namespace Blocks {
-	public class MultiHit : Brick {
-		[SerializeField] private int maxHp = 2;
-		private int hp = -1;
+    public class MultiHit : Brick {
+        [SerializeField] private int maxHp = 2;
+        private int hp = -1;
 
-		[SerializeField] private Sprite[] sprites = null;
+        [SerializeField] private Sprite[] sprites = null;
 
-		public override void Awake() {
-			base.Awake();
-			hp = maxHp;
-			Debug.Assert(sprites.Length == maxHp);
-			ren.sprite = sprites[0];
-		}
+        public override void Awake() {
+            base.Awake();
+            hp = maxHp;
+            Debug.Assert(sprites.Length == maxHp);
+            ren.sprite = sprites[0];
+        }
 
-		public override void Hit(IActor maker) {
-			base.Hit(maker);
-			hp--;
-			if (hp <= 0) {
-				Break();
-			}
-			else {
-				ren.sprite = sprites[maxHp - hp];
-			}
-		}
-	}
+        public override void Hit(IActor maker) {
+            base.Hit(maker);
+            if (ColorsMatch(maker)) {
+                hp--;
+                if (hp <= 0) {
+                    Break();
+                }
+                else {
+                    ren.sprite = sprites[maxHp - hp];
+                }
+            }
+        }
+    }
 }
