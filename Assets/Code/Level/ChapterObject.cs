73cfb555cbe539f028fefc67d68192e15e12aa8b
Janek Winkler <janekx21@gmail.com>
2020-02-26 16:18:31 +0100

    make level add to chapter

--- Assets/Code/Level/ChapterObject.cs
+++ Assets/Code/Level/ChapterObject.cs
@@ -2,6 +2,8 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Assertions;
+using Util;
 
 namespace Level {
     [CreateAssetMenu(fileName = "new ChapterObject", menuName = "ChapterObject", order = 0)]
@@ -9,20 +11,46 @@ namespace Level {
         public Sprite image = null;
         public LevelObject[] levels = new LevelObject[0];
 
-		[ContextMenu("Find All Levels")]
-		private void FindAllLevelObjects() {
+        private string directory {
+            get {
+                var path = AssetDatabase.GetAssetPath(this);
+                return Path.GetDirectoryName(path);
+            }
+        }
+
+        [ContextMenu("Find All Levels")]
+        private void FindAllLevelObjects() {
             List<LevelObject> levelList = new List<LevelObject>();
-            var path = AssetDatabase.GetAssetPath(this);
-            var dir = Path.GetDirectoryName(path);
-            
-			var allLevelPaths = AssetDatabase.FindAssets("t:LevelObject", new []{dir});
-			foreach (var guid in allLevelPaths) {
-				var levelPath = AssetDatabase.GUIDToAssetPath(guid);
-				var lo = AssetDatabase.LoadAssetAtPath<LevelObject>(levelPath);
-				levelList.Add(lo);
-			}
+
+            var allLevelPaths = AssetDatabase.FindAssets("t:LevelObject", new[] {directory});
+            foreach (var guid in allLevelPaths) {
+                var levelPath = AssetDatabase.GUIDToAssetPath(guid);
+                var lo = AssetDatabase.LoadAssetAtPath<LevelObject>(levelPath);
+                levelList.Add(lo);
+            }
 
             levels = levelList.ToArray();
+        }
+
+        [ContextMenu("Make Level")]
+        public void MakeLevel() {
+            var guid = AssetDatabase.CreateFolder(directory, "Level01");
+            var path = AssetDatabase.GUIDToAssetPath(guid);
+            
+            Assert.IsNotNull(path);
+
+            var levelName = Path.GetFileName(path);
+
+            var scenePath = Path.Combine(path, $"{levelName}.unity");
+            var worked = AssetDatabase.CopyAsset("Assets/Scenes/Empty.unity", scenePath);
+
+            Assert.IsTrue(worked);
+
+            var obj = CreateInstance<LevelObject>();
+            obj.scene = new SceneReference {ScenePath = scenePath};
+            AssetDatabase.CreateAsset(obj, Path.Combine(path, $"{levelName}.asset"));
+            
+            FindAllLevelObjects();
         }
     }
 }
--- Assets/Code/UI/Menu/Menu.cs
+++ Assets/Code/UI/Menu/Menu.cs
@@ -18,19 +18,6 @@ namespace UI.Menu {
 			list.Init(chapterObject.levels, changeEvent);
 		}
 
-		[ContextMenu("Find All Levels")]
-		private void FindAllLevelObjects() {
-            List<LevelObject> levelList = new List<LevelObject>();
-			var allLevelPaths = AssetDatabase.FindAssets("t:LevelObject");
-			foreach (var guid in allLevelPaths) {
-				var levelPath = AssetDatabase.GUIDToAssetPath(guid);
-				var lo = AssetDatabase.LoadAssetAtPath<LevelObject>(levelPath);
-				levelList.Add(lo);
-			}
-
-            chapterObject.levels = levelList.ToArray();
-        }
-
 		private void LoadLevel(LevelObject level) {
 			SceneManager.LoadScene(level.scene.ScenePath);
 		}
