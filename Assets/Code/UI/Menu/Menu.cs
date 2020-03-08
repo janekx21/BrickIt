5ed9c0f215ee042974747aed9731c0a90e4dc665
Janek Winkler <janekx21@gmail.com>
2020-03-08 18:11:14 +0100

    add new menu function

--- Assets/Code/UI/Menu/Menu.cs
+++ Assets/Code/UI/Menu/Menu.cs
@@ -4,20 +4,48 @@ using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 namespace UI.Menu {
-	public class Menu : MonoBehaviour {
-		[SerializeField] private ChapterContainerObject chapterContainerObject = null;
-		[SerializeField] private ChapterList chapterList = null;
+    public class Menu : MonoBehaviour {
+        enum State {
+            Chapter,
+            Level
+        }
 
-        public class OnChapterAction : UnityEvent<ChapterObject>{}
-        
-		private void Awake() {
+
+        [SerializeField] private ChapterContainerObject chapterContainerObject = null;
+        [SerializeField] private ChapterList chapterList = null;
+        [SerializeField] private LevelList levelList = null;
+        [SerializeField] private Animator animator = null;
+
+        private State currentState = State.Chapter;
+        private static readonly int ChapterHash = Animator.StringToHash("chapter");
+        private static readonly int LevelHash = Animator.StringToHash("level");
+
+        public class OnChapterAction : UnityEvent<ChapterObject> {
+        }
+
+        private void Awake() {
             var changeEvent = new OnChapterAction();
             changeEvent.AddListener(LoadChapter);
-			chapterList.Init(chapterContainerObject.chapters , changeEvent);
-		}
+            chapterList.Init(chapterContainerObject.chapters, changeEvent);
+        }
 
-		private void LoadChapter(ChapterObject chapter) {
-			SceneManager.LoadScene(chapter.scene.ScenePath);
-		}
-	}
+        private void Update() {
+            if (Input.GetButtonDown("Cancel") && currentState == State.Level) {
+                animator.SetTrigger(ChapterHash);
+                currentState = State.Chapter;
+            }
+        }
+
+        private void LoadChapter(ChapterObject chapter) {
+            var action = new ChapterMenu.OnLevelAction();
+            action.AddListener(LoadLevel);
+            levelList.Init(chapter.levels, action);
+            animator.SetTrigger(LevelHash);
+            currentState = State.Level;
+        }
+
+        private void LoadLevel(LevelObject level) {
+            SceneManager.LoadScene(level.scene);
+        }
+    }
 }
