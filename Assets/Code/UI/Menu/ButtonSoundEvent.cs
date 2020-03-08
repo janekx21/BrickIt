e2cd489f3d4a4b8ac8dc460c23b7b5c8a4929d08
Janek Winkler <janekx21@gmail.com>
2020-03-08 21:19:40 +0100

    add abstract sound effects for buttons

---
+++ Assets/Code/UI/Menu/ButtonSoundEvent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Util;
+
+namespace UI.Menu {
+    
+    [RequireComponent(typeof(SoundParticle))]
+    public class ButtonSoundEvent : MonoBehaviour, IPointerEnterHandler,  ISubmitHandler, IPointerClickHandler {
+        [SerializeField] private AudioClip hover = null;
+        [SerializeField] private AudioClip click = null;
+        private SoundParticle source = null;
+
+        private void Awake() {
+            source = GetComponent<SoundParticle>();
+        }
+
+        public void OnPointerEnter(PointerEventData ped) {
+            source.OneShot(hover);
+        }
+
+        public void OnSubmit(BaseEventData eventData) {
+            source.OneShot(click);
+        }
+
+        public void OnPointerClick(PointerEventData eventData) {
+            source.OneShot(click);
+        }
+    }
+}
