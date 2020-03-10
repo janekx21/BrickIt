908e861ca6b2dc73c0b04bc16254023759abb5a1
Janek Winkler <janekx21@gmail.com>
2020-03-10 08:23:13 +0100

    add new levels

--- Assets/Code/Blocks/DirectionChanger.cs
+++ Assets/Code/Blocks/DirectionChanger.cs
@@ -3,16 +3,18 @@ using UnityEngine;
 using UnityEngine.Events;
 
 namespace Blocks {
-	public class DirectionChanger : Block, IInteractable {
+    public class DirectionChanger : Block, IInteractable {
         [SerializeField] UnityEvent onInteract = new UnityEvent();
-		public override void Hit(IActor actor) {
-			base.Hit(actor);
 
-			actor.ChangeDirection();
-		}
+        public override void Hit(IActor actor) {
+            base.Hit(actor);
+            if (ColorsMatch(actor)) {
+                actor.ChangeDirection();
+            }
+        }
 
-		protected override bool shouldBreak() => false;
-        
+        protected override bool shouldBreak() => false;
+
         public void Interact(IActor _) => onInteract.Invoke();
     }
 }
