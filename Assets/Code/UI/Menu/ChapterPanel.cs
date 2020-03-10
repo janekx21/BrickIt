a7017a99b250febf4b48f8ca2c2707b99fbe9468
Janek Winkler <janekx21@gmail.com>
2020-03-10 20:36:57 +0100

    add chapter image

--- Assets/Code/UI/Menu/ChapterPanel.cs
+++ Assets/Code/UI/Menu/ChapterPanel.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 namespace UI.Menu {
 	public class ChapterPanel : MonoBehaviour {
 		[SerializeField] private Text chapterName = null;
-		[SerializeField] private Sprite image = null;
+		[SerializeField] private Image image = null;
 		[SerializeField] private Button button = null;
 
 		public void Init(ChapterObject chapterObject, Menu.OnChapterAction loadAction) {
 			chapterName.text = chapterObject.name;
-			image = chapterObject.image;
+			image.sprite = chapterObject.image;
 			button.onClick.AddListener(() => loadAction.Invoke(chapterObject));
 		}
 	}
