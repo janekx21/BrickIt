c5b54dc68855da154cb1cd0e86aa2f19b68f330f
Janek Winkler <janekx21@gmail.com>
2020-03-12 20:06:34 +0100

    add ingame panel

---
+++ Assets/Code/UI/LevelTimer.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI {
+    public class LevelTimer : MonoBehaviour {
+        [SerializeField] private Text text;
+
+        private void Update() {
+            text.text = $"{Level.Level.Own.TimeSinceStart:000}";
+        }
+    }
+}
