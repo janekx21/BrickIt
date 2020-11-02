59476c00e5891197f2eea308bb8271fc1c8eb475
Maico Weise <silevol@gmail.com>
2020-11-02 20:11:45 +0100

    reworked pixel perfect camera for npp ui

---
+++ Assets/Code/Graphics/ViewportCopy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Graphics {
+    
+    [ExecuteInEditMode]
+    [RequireComponent(typeof(Camera))]
+    public class ViewportCopy : MonoBehaviour {
+        [SerializeField] private GameObject parentCamera = null;
+
+        private Camera parentCam;
+        private Camera cam;
+
+        void Awake() {
+            cam = GetComponent<Camera>();
+            parentCam = parentCamera.GetComponent<Camera>();
+            LateUpdate();
+        }
+
+        void LateUpdate() {
+            cam.orthographicSize = parentCam.orthographicSize;
+            cam.rect = parentCam.rect;
+        }
+    }
+}
