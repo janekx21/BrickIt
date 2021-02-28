985d9fdf5c1972dcc014c297740e31cb63b8ec77
Maico Weise <silevol@gmail.com>
2021-02-28 15:05:09 +0100

    added controller support to chapter and levelselection

--- Assets/Code/UI/Menu/ChapterList.cs
+++ Assets/Code/UI/Menu/ChapterList.cs
@@ -1,11 +1,27 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using LevelContext;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace UI.Menu {
     public class ChapterList : MonoBehaviour {
         [SerializeField] private ChapterPanel prefab = null;
+
+        [Header("Navigation")]
+        [SerializeField] private EventSystem eventSystem = null;
+        [SerializeField] private GameObject HorizontalScrollbar = null;
+        private Selectable lastPanelButton = null;
+        private Selectable beforeLastPanelButton = null;
+
+        private Selectable firstPanel = null;
+        
+        public void MarkFirstChapter() {
+            firstPanel.Select();
+            firstPanel.OnSelect(null);
+        }
 
         public void Init(ChapterObject[] chapterObjects, Menu.OnChapterAction loadAction) {
             foreach (Transform t in transform) {
@@ -17,6 +33,46 @@ namespace UI.Menu {
             foreach (var o in list) {
                 var panel = Instantiate(prefab, transform);
                 panel.Init(o, loadAction);
+
+                // Navigation
+                if (o == list.First()) {
+                    firstPanel = panel.GetComponent<Button>();
+                    MarkFirstChapter();
+
+                    lastPanelButton = panel.GetComponent<Button>();
+                }
+                else if (o == list[1]) {
+                    Navigation navigation = new Navigation {
+                        mode = Navigation.Mode.Explicit,
+                        selectOnDown = HorizontalScrollbar.GetComponent<Scrollbar>(),
+                        selectOnRight = panel.GetComponent<Button>()
+                    };
+                    lastPanelButton.navigation = navigation;
+
+                    beforeLastPanelButton = lastPanelButton;
+                    lastPanelButton = panel.GetComponent<Button>();
+                }
+                else {
+                    Navigation navigation = new Navigation {
+                        mode = Navigation.Mode.Explicit,
+                        selectOnDown = HorizontalScrollbar.GetComponent<Scrollbar>(),
+                        selectOnLeft = beforeLastPanelButton,
+                        selectOnRight = panel.GetComponent<Button>()
+                    };
+                    lastPanelButton.navigation = navigation;
+
+                    beforeLastPanelButton = lastPanelButton;
+                    lastPanelButton = panel.GetComponent<Button>();
+                }
+                
+                if (o == list.Last()) {
+                    Navigation navigation = new Navigation {
+                        mode = Navigation.Mode.Explicit,
+                        selectOnDown = HorizontalScrollbar.GetComponent<Scrollbar>(),
+                        selectOnLeft = beforeLastPanelButton
+                    };
+                    panel.GetComponent<Button>().navigation = navigation;
+                }
             }
         }
 
