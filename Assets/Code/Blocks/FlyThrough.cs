c7fec3623e8b2c2841f270e5b6a6f2ea4be69453
Maico Weise <silevol@gmail.com>
2021-03-31 00:12:35 +0200

    added FlyThroughBlock

---
+++ Assets/Code/Blocks/FlyThrough.cs
@@ -0,0 +1,21 @@
+﻿using GamePlay;
+
+namespace Blocks {
+    public class FlyThrough : Brick, IInteractable {
+        public override void Over(IActor actor) {
+            base.Over(actor);
+
+            if (ColorsMatch(actor)) {
+                Break(actor);
+                actor.Combo();
+            }
+            else {
+                boxCollider.isTrigger = false;
+            }
+        }
+
+        public void Interact(IActor actor) {
+            boxCollider.isTrigger = true;
+        }
+    }
+}
