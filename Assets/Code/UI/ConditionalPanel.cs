da324bd2b9012f0c49db293d42031ed762a2db1a
Janek Winkler <janekx21@gmail.com>
2022-09-29 12:18:14 +0200

    animations

--- Assets/Code/UI/ConditionalPanel.cs
+++ Assets/Code/UI/ConditionalPanel.cs
@@ -1,18 +1,39 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 using LevelContext;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace UI {
+    [RequireComponent(typeof(CanvasGroup))]
     public class ConditionalPanel : Panel {
         [SerializeField] private LevelState ownState = LevelState.Begin;
+        [SerializeField] private bool useAnimation;
+        [SerializeField] private AnimationCurve alphaCurve = new();
+        [SerializeField] private AnimationCurve scaleCurve = new();
+
+        private CanvasGroup group;
 
         private void Start() {
             OnUpdateLevelState(Level.Own.State);
             Level.Own.onStateChanged.AddListener(OnUpdateLevelState);
+            group = GetComponent<CanvasGroup>();
         }
 
         public override void OnUpdateLevelState(LevelState state) {
             gameObject.SetActive(state == ownState);
+            if (useAnimation) {
+                StartCoroutine(FadeRoutine());
+            }
+        }
+
+        IEnumerator FadeRoutine() {
+            for (float t = 0; t < alphaCurve.keys.Last().time; t+=Time.deltaTime) {
+                group.alpha = alphaCurve.Evaluate(t);
+                transform.localScale = scaleCurve.Evaluate(t) * Vector3.one;
+                yield return null;
+            }
         }
     }
 }
